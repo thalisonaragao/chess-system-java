631e309cb16d42f48b31d78e3bf5d79966cf84da
Thalison Aragão <thalissonaragao01@gmail.com>
2025-01-14 18:30:45 -0300

    Checkmate logic

--- chess-system/src/application/Main.java
+++ chess-system/src/application/Main.java
@@ -16,7 +16,7 @@ public class Main {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true){
+        while (!chessMatch.getCheckMate()){
             try {
                 UI.clearScreen();
                 UI.printMatch(chessMatch,captured);
@@ -45,5 +45,7 @@ public class Main {
                 System.out.println(e.getMessage());
             }
         }
+        UI.clearScreen();
+        UI.printMatch(chessMatch,captured);
     }
 }
